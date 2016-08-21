497dd5e5b5fcfdf27d0b6a7698a67e9ffd99b5ba
Mary Beth Baker <MaryBethBaker@users.noreply.github.com>
2016-08-21 17:33:00 -0400

    updated mechanize code
    added tested mechanize code for pulling an h1 from a page.

--- scaper.rb
+++ scaper.rb
@@ -1,22 +1,26 @@
 # get Ruby gems that are useful for scraping
 require 'mechanize'
 # require 'Httparty'
-require 'nokogiri'
+# require 'nokogiri'
 # require 'open-uri'
 # require 'JSON'
 # require 'Pry'
 # require 'csv'
 
+mechanize = Mechanize.new
+
 # make a place to save scraped data
 
-# i think this opens the HTML page above
+# visit the following HTML page
+page = mechanize.get('http://pm.gc.ca/eng/leader-government-house-commons-mandate-letter')
 
 # go to each link in the body/main
 
 # on each link find the following data
   
 # put the titles and bullet points into a JSON file named above
-
+# prints the title
+puts page.title
 # do this until all 30 pages have been scraped, then end the program.
 
 
