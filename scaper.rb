6d0330860b64c36f8d520f91297ad57af979b4e1
Mary Beth Baker <MaryBethBaker@users.noreply.github.com>
2016-08-21 09:53:36 -0400

    updates
    trying many tutorials - looks like a mess right now

--- scaper.rb
+++ scaper.rb
@@ -1,24 +1,29 @@
+# get Ruby gems that are useful for scraping
 require 'Httparty'
 require 'nokogiri'
 require 'open-uri'
 require 'JSON'
 require 'Pry'
-require 'csv'
+# require 'csv'
 
-DATA_DIR = "data-hold/committments"
+# make a place to save scraped data
+DATA_DIR = "committments"
 Dir.mkdir(DATA_DIR) unless File.exists?(DATA_DIR)
 
-#this is the place we're going to scrape
-BASE_URL = 'http://pm.gc.ca'
-BASE_DIR = '/eng/ministerial-mandate-letters'
-#Then go to each of the mandate letters
-LOCAL_DIR = '/* mandate-letter'
+#this is the place we're going to crawl and scrape data from (do I need lines 14/15)
+#BASE_URL = 'http://pm.gc.ca'
+#BASE_DIR = '/eng/ministerial-mandate-letters'
 
 # find out what this line does
-HEADERS_HASH = {"User-Agent" => "Ruby/#{RUBY_VERSION}"}
+#HEADERS_HASH = {"User-Agent" => "Ruby/#{RUBY_VERSION}"}
 
-page = Nokogiri::HTML(open(LIST_URL))
+# i think this opens the HTML page above
+doc = Nokogiri::HTML(open("http://www.pm.gc.ca/eng/ministerial-mandate-letters"))
+@doc.xpath("//H1")
+@doc.xpath("//li")
+ #What does the Nokogiri for rows = do?
  #rows = page.css('div.mw-content-ltr table.wikitable tr')
+
 
 # Need to modify information here
 rows[1..-2].each do |row|
